ddde3148a3f3fba02aa2b3af4a23906f7d3018d5
Jesse Gonzalez <jessegonzalez024@gmail.com>
2019-11-25 06:25:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,10 @@
 class Student < User
   attr_accessor :Knowledge
   
-    def initialize(first_name = nil, last_name = nil)
+  def initialize(first_name = nil, last_name = nil)
     @first_name = first_name
     @last_name = last_name
+    @Knowledge = []
   end
   def learn (string)
     @Knowledge << string
